df75d2cdb0317161b66264428fb69fbcfeeee897
WoeunJung <leena09123@naver.com>
2024-07-29 21:53:08 +0900

    feat: add presentation module

---
+++ presentation/src/main/java/com/wasin/presentation/signup/SignupScreen.kt
@@ -0,0 +1,11 @@
+package com.wasin.presentation.signup
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SignupScreen() {
+    Text(
+        text = "Sign up screen"
+    )
+}
