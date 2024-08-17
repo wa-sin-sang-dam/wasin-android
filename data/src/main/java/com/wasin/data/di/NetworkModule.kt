48a9376d3c55362194c558f12b270f2cdbe0d84e
1jeongg <leena0912@pusan.ac.kr>
2024-08-18 00:55:30 +0900

    feat: add network module (+ dependency injection)

---
+++ data/src/main/java/com/wasin/data/di/NetworkModule.kt
@@ -0,0 +1,151 @@
+package com.wasin.data.di
+
+import android.content.Context
+import android.util.Log
+import com.wasin.data._const.DataStoreKey
+import com.wasin.data._const.HttpRoutes
+import com.wasin.data.api.UserApi
+import com.wasin.data.data_api.UserRepository
+import com.wasin.data.datastore.WasinDataStore
+import com.wasin.data.model.user.ReissueRequest
+import com.wasin.data.model.user.ReissueResponse
+import com.wasin.data.repository.UserRepositoryImpl
+import com.wasin.data.util.ApiUtils
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.RefreshTokensParams
+import io.ktor.client.plugins.auth.providers.bearer
+import io.ktor.client.plugins.compression.ContentEncoding
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.HttpHeaders
+import io.ktor.http.URLProtocol
+import io.ktor.http.contentType
+import io.ktor.http.path
+import io.ktor.serialization.kotlinx.json.json
+import io.ktor.util.appendIfNameAbsent
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    private val accessKey = DataStoreKey.ACCESS_TOKEN_KEY.name
+    private val refreshKey = DataStoreKey.REFRESH_TOKEN_KEY.name
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(
+        @ApplicationContext context: Context
+    ): HttpClient {
+        return HttpClient(CIO) {
+            install(Logging) {
+                logger = object: Logger {
+                    override fun log(message: String) {
+                        Log.d("wasin_api", message)
+                    }
+                }
+                level = LogLevel.ALL
+            }
+            install(ContentNegotiation) {
+                json(Json{
+                    prettyPrint = true
+                    isLenient = true
+                    encodeDefaults = true
+                })
+            }
+            install(HttpTimeout) {
+                connectTimeoutMillis = 8000
+                requestTimeoutMillis = 8000
+                socketTimeoutMillis = 8000
+            }
+            install(Auth) {
+                bearer {
+                    refreshTokens {
+                        val token = getRefreshToken(context)
+                        token
+                    }
+                }
+            }
+            install(ContentEncoding) {
+                deflate(1.0F)
+                gzip(0.9F)
+            }
+            defaultRequest{
+                contentType(ContentType.Application.Json)
+                url {
+                    protocol = URLProtocol.HTTP
+                    host = HttpRoutes.BASE_HOST.path
+                    port = 8080
+                    path(HttpRoutes.BASE_V1_PATH.path)
+                }
+                val accessToken = WasinDataStore(context).getData(accessKey)
+                if (accessToken.isNotEmpty()) {
+                    headers.appendIfNameAbsent(HttpHeaders.Authorization, accessToken)
+                }
+            }
+        }
+    }
+
+    private suspend fun RefreshTokensParams.getRefreshToken(context: Context): BearerTokens? {
+        val refreshToken = WasinDataStore(context).getData(refreshKey)
+        val apiResult = client
+            .post(HttpRoutes.REISSUE.getPath1()) {
+                setBody(ReissueRequest(refreshToken))
+                markAsRefreshTokenRequest()
+            }.body<ApiUtils.ApiResult<ReissueResponse>>()
+
+        return if (!apiResult.success) null
+            else getBearerTokens(apiResult, context)
+    }
+
+    private fun getBearerTokens(
+        apiResult: ApiUtils.ApiResult<ReissueResponse>,
+        context: Context
+    ): BearerTokens {
+        val newAccessToken = apiResult.response?.accessToken ?: ""
+        val newRefreshToken = apiResult.response?.refreshToken ?: ""
+
+        WasinDataStore(context).setData(accessKey, newAccessToken)
+        WasinDataStore(context).setData(refreshKey, newRefreshToken)
+
+        return BearerTokens(
+            accessToken = newAccessToken.parseToken(),
+            refreshToken = newRefreshToken.parseToken()
+        )
+    }
+
+    private fun String.parseToken(): String {
+        if (this.startsWith("Bearer ")) {
+            return this.substring(7)
+        }
+        return this
+    }
+
+    @Provides
+    @Singleton
+    fun provideUserApi(client: HttpClient): UserRepository {
+        return UserApi(client)
+    }
+    @Provides
+    @Singleton
+    fun provideUserRepositoryImpl(userApi: UserApi): UserRepositoryImpl {
+        return UserRepositoryImpl(userApi)
+    }
+}
