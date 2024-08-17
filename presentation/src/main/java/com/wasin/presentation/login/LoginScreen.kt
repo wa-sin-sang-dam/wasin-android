1b1aa40119c4d2586114f1ab4fd59ef54fc5a901
1jeongg <leena0912@pusan.ac.kr>
2024-08-18 00:57:46 +0900

    feat: login api connection in presentation layer

--- presentation/src/main/java/com/wasin/presentation/login/LoginScreen.kt
+++ presentation/src/main/java/com/wasin/presentation/login/LoginScreen.kt
@@ -12,27 +12,46 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.wasin.data.model.user.LoginRequest
 import com.wasin.presentation.R
 import com.wasin.presentation._common.BlueLongButton
 import com.wasin.presentation._common.TextField
 import com.wasin.presentation._common.WhiteLongButton
 import com.wasin.presentation._navigate.WasinScreen
+import com.wasin.presentation._util.LaunchedEffectEvent
 
 @Composable
 fun LoginScreen(
     navController: NavController,
-    onNavigate: () -> Unit = {}
+    nextScreen: String,
+    viewModel: LoginViewModel = hiltViewModel()
 ) {
+    LaunchedEffectEvent(
+        eventFlow = viewModel.eventFlow,
+        onNavigate = {
+            viewModel.saveScreenState(nextScreen)
+            navController.navigate(nextScreen)
+        }
+    )
     LazyColumn (
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         item { WasinLogo() }
-        item { LoginTextField() }
-        item { LoginButton(onNavigate) }
+        item {
+            LoginTextField(
+                loginDTO = viewModel.loginDTO.value,
+                onEmail = { viewModel.onEvent(LoginEvent.EnterEmail(it)) },
+                onPassword = { viewModel.onEvent(LoginEvent.EnterPassword(it)) }
+            )
+        }
+        item { LoginButton { viewModel.onEvent(LoginEvent.Login) } }
         item { SignupButton{ navController.navigate(WasinScreen.SignupScreen.route) } }
     }
 }
@@ -47,20 +66,27 @@ fun WasinLogo() {
 }
 
 @Composable
-fun LoginTextField() {
+fun LoginTextField(
+    loginDTO: LoginRequest,
+    onEmail: (String) -> Unit = {},
+    onPassword: (String) -> Unit = {}
+) {
     Column(
         modifier = Modifier.padding(top = 65.dp, bottom = 53.dp)
     ) {
         TextField(
-            modifier = Modifier.fillMaxWidth()
-                .padding(bottom = 14.dp),
-            text = "",
+            modifier = Modifier.fillMaxWidth().padding(bottom = 14.dp),
+            text = loginDTO.email,
             placeholder = "아이디(이메일)",
+            onValueChange = onEmail,
+            keyboardType = KeyboardType.Email
         )
         TextField(
             modifier = Modifier.fillMaxWidth(),
-            text = "",
+            text = loginDTO.password,
             placeholder = "비밀번호",
+            onValueChange = onPassword,
+            visualTransformation = PasswordVisualTransformation()
         )
     }
 }
