fa944b982ea2f43ae1116e08c5b58631fd207eaa
1jeongg <leena0912@pusan.ac.kr>
2024-08-22 14:55:23 +0900

    feat: emit profile data in presentation layer

--- presentation/src/main/java/com/wasin/presentation/profile/ProfileScreen.kt
+++ presentation/src/main/java/com/wasin/presentation/profile/ProfileScreen.kt
@@ -15,50 +15,71 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.wasin.data.model.profile.FindAllProfileResponse
 import com.wasin.presentation._common.WithTitle
 import com.wasin.presentation._theme.gray_808080
+import com.wasin.presentation._theme.gray_B8B8B8
 import com.wasin.presentation._theme.gray_E8E8E8
 import com.wasin.presentation._theme.light_blue
 import com.wasin.presentation._theme.main_blue
 import com.wasin.presentation._theme.typography
+import com.wasin.presentation._util.LaunchedEffectEvent
 
 @Composable
-fun ProfileScreen(navController: NavController) {
+fun ProfileScreen(
+    navController: NavController,
+    viewModel: ProfileViewModel = hiltViewModel()
+) {
+    LaunchedEffectEvent(eventFlow = viewModel.eventFlow)
     WithTitle(
         title = "프로파일 설정",
         description = "프로파일에 따라 여러 공유기의 설정을 한 번에 변경할 수 있어요. 자동 변경일 경우 상황에 따라 최적의 프로파일을 시스템이 알아서 찾아서 변경해줘요."
     ) {
-        item { ProfileModeSelect() }
-        item { ProfileSelect() }
+        item {
+            ProfileModeSelect(
+                isAuto = viewModel.profileDTO.value.isAuto,
+                changeModeAuto = { viewModel.changeModeAuto() },
+                changeModeManual = { viewModel.changeModeManual() }
+            )
+        }
+        item {
+            ProfileSelect(
+                enabled = !viewModel.profileDTO.value.isAuto,
+                activeProfileId = viewModel.profileDTO.value.activeProfileId,
+                profileList = viewModel.profileDTO.value.profiles,
+                changeProfile = { viewModel.changeProfile(it) }
+            )
+        }
     }
 }
 
 @Composable
-fun ProfileModeSelect() {
-    val isAuto = remember { mutableStateOf(false) }
+fun ProfileModeSelect(
+    isAuto: Boolean,
+    changeModeAuto: () -> Unit,
+    changeModeManual: () -> Unit,
+) {
     Row(
         modifier = Modifier.fillMaxWidth()
     ) {
         ProfileMode(
             modifier = Modifier.weight(1f),
             text = "프로파일 자동 변경",
-            isSelected = isAuto.value,
-            onClick = { isAuto.value = true }
+            isSelected = isAuto,
+            onClick = changeModeAuto
         )
         Spacer(modifier = Modifier.width(10.dp))
         ProfileMode(
             Modifier.weight(1f),
             text = "프로파일 수동 변경",
-            isSelected = !isAuto.value,
-            onClick = { isAuto.value = false }
+            isSelected = !isAuto,
+            onClick = changeModeManual
         )
     }
 }
@@ -97,8 +118,12 @@ private fun selectedStyle(isSelected: Boolean): Pair<BorderStroke, Color> {
 
 
 @Composable
-fun ProfileSelect() {
-    val isSelected = remember { mutableIntStateOf(0) }
+fun ProfileSelect(
+    enabled: Boolean,
+    activeProfileId: Long,
+    changeProfile: (Long) -> Unit,
+    profileList: List<FindAllProfileResponse.ProfileEachDTO>
+) {
     Column {
         Text(
             text = "프로파일",
@@ -110,8 +135,16 @@ fun ProfileSelect() {
             style = typography.bodyLarge,
             modifier = Modifier.padding(top = 11.dp, bottom = 21.dp)
         )
-        ProfileComponent()
-        ProfileComponent(isSelected = true)
+        profileList.forEach {
+            ProfileComponent(
+                title = it.title,
+                description = it.description,
+                tip = it.tip,
+                isSelected = it.profileId == activeProfileId,
+                onClick = { changeProfile(it.profileId) },
+                enabled = enabled
+            )
+        }
     }
 }
 
@@ -122,15 +155,21 @@ fun ProfileComponent(
     description: String = "트래픽이 하나의 와이파이에 몰리면 사용자 수를 제한해줘요.",
     tip: String = "⭐ 축제 등 사용자가 갑자기 늘어날 때 사용하기 좋아요!",
     isSelected: Boolean = false,
+    enabled: Boolean = false,
     onClick: () -> Unit = {}
 ) {
-    val (border, background) = selectedStyle(isSelected)
+    var (border, background) = selectedStyle(isSelected)
+    if (!enabled) {
+        border = BorderStroke(1.dp, gray_E8E8E8)
+        background = gray_E8E8E8
+    }
+
     Column(
         modifier = modifier
             .fillMaxWidth()
             .padding(bottom = 14.dp)
             .clip(RoundedCornerShape(30.dp))
-            .clickable(onClick = onClick)
+            .clickable(enabled = enabled, onClick = onClick)
             .border(border, RoundedCornerShape(30.dp))
             .background(background)
             .padding(20.dp),
@@ -138,17 +177,18 @@ fun ProfileComponent(
     ) {
         Text(
             text = title,
-            style = typography.titleSmall
+            style = typography.titleSmall,
+            color = if (enabled) Color.Black else gray_B8B8B8
         )
         Text(
             text = description,
             style = typography.bodyLarge,
-            color = gray_808080,
+            color = if (enabled) gray_808080 else gray_B8B8B8,
         )
         Text(
             text = tip,
             style = typography.bodySmall,
-            color = gray_808080,
+            color = if (enabled) gray_808080 else gray_B8B8B8,
             modifier = Modifier.padding(top = 15.dp)
         )
     }
---
+++ presentation/src/main/java/com/wasin/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,108 @@
+package com.wasin.presentation.profile
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wasin.data.model.profile.FindAllProfileResponse
+import com.wasin.domain.usecase.profile.ChangeModeAuto
+import com.wasin.domain.usecase.profile.ChangeModeManual
+import com.wasin.domain.usecase.profile.ChangeProfile
+import com.wasin.domain.usecase.profile.FindAllProfile
+import com.wasin.domain.utils.Resource
+import com.wasin.presentation._util.WasinEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ProfileViewModel @Inject constructor(
+    private val findAllProfileUseCase: FindAllProfile,
+    private val changeModeAutoUseCase: ChangeModeAuto,
+    private val changeModeManualUseCase: ChangeModeManual,
+    private val changeProfileUseCase: ChangeProfile
+): ViewModel() {
+
+    private val _profileDTO = mutableStateOf(FindAllProfileResponse())
+    val profileDTO = _profileDTO
+
+    private val _eventFlow = MutableSharedFlow<WasinEvent>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    init {
+        findAllProfile()
+    }
+
+    fun changeProfile(profileId: Long) {
+        viewModelScope.launch {
+            if (profileId == profileDTO.value.activeProfileId) {
+                _eventFlow.emit(WasinEvent.MakeToast("이미 해당 프로파일은 선택되었습니다."))
+                return@launch
+            }
+            changeProfileUseCase(profileId).collect { response ->
+                when (response) {
+                    is Resource.Loading -> _eventFlow.emit(WasinEvent.Loading)
+                    is Resource.Error -> _eventFlow.emit(WasinEvent.MakeToast(response.message))
+                    is Resource.Success -> {
+                        _profileDTO.value = _profileDTO.value.copy(
+                            activeProfileId = profileId
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun changeModeAuto() {
+        viewModelScope.launch {
+            if (profileDTO.value.isAuto) {
+                _eventFlow.emit(WasinEvent.MakeToast("이미 자동 모드입니다."))
+                return@launch
+            }
+            changeModeAutoUseCase().collect { response ->
+                when (response) {
+                    is Resource.Loading -> _eventFlow.emit(WasinEvent.Loading)
+                    is Resource.Error -> _eventFlow.emit(WasinEvent.MakeToast(response.message))
+                    is Resource.Success -> {
+                        _profileDTO.value = _profileDTO.value.copy(
+                            isAuto = true
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun changeModeManual() {
+        viewModelScope.launch {
+            if (!profileDTO.value.isAuto) {
+                _eventFlow.emit(WasinEvent.MakeToast("이미 수동 모드입니다."))
+                return@launch
+            }
+            changeModeManualUseCase().collect { response ->
+                when (response) {
+                    is Resource.Loading -> _eventFlow.emit(WasinEvent.Loading)
+                    is Resource.Error -> _eventFlow.emit(WasinEvent.MakeToast(response.message))
+                    is Resource.Success -> {
+                        _profileDTO.value = _profileDTO.value.copy(
+                            isAuto = false
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    private fun findAllProfile() {
+        viewModelScope.launch {
+            findAllProfileUseCase().collect { response ->
+                when (response) {
+                    is Resource.Loading -> _eventFlow.emit(WasinEvent.Loading)
+                    is Resource.Error -> _eventFlow.emit(WasinEvent.MakeToast(response.message))
+                    is Resource.Success -> _profileDTO.value = response.data ?: FindAllProfileResponse()
+                }
+            }
+        }
+    }
+}
