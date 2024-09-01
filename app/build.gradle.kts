272b202cc1cff5cf00902ed088138aefd5ea5e92
1jeongg <leena0912@pusan.ac.kr>
2024-09-01 20:52:37 +0900

    chore: add dependency and module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,13 @@ android {
 dependencies {
 
     implementation(project(":presentation"))
+    implementation(project(":domain"))
+    implementation(project(":data"))
 
     implementation(libs.hilt.android)
     implementation(libs.androidx.work.runtime.ktx)
+    implementation(libs.androidx.hilt.work)
+    kapt(libs.androidx.hilt.compiler)
     kapt(libs.hilt.compiler)
     implementation(libs.androidx.hilt.navigation.compose)
 
